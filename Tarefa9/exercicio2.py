e6124cd191843e4c1460980251f5abe8f51d11ff
cabralsembarco <cabral.pedro@escolar.ifrn.edu.br>
2026-08-11 17:20:55 +0000

    commit

---
+++ Tarefa9/exercicio2.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+class Service:
+    @staticmethod
+    def cliente_inserir(____):
+        obj = ____
+        ClienteDAO.inserir(obj)
+    
+    @staticmethod
+    def cliente_listar(____):
+        ____
+    @staticmethod
+    def cliente_listar_id(id):
+        ____
+    @staticmethod
+    def cliente_atualizar(____):
+        ____
+
+        ClienteDAO().atualizar(obj)
+        
+    @staticmethod
+    def cliente_exxcluir(id)
+        ClienteDAO().excluir(obj)
